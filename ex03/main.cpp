bff4a20b45f9a287b28ef0604a35337bdd4214ce
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-09-15 14:45:12 +0200

    ex03 seems ok

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -3,18 +3,7 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
-
-class Intern
-{
-	public :
-		Intern();
-		Intern(const Intern &src);
-		~Intern();
-
-		Intern &operator=(const Intern &rhs);
-
-		AForm *makeForm(std::string formName, std::string target);
-};
+#include "Intern.hpp"
 
 Intern::Intern(){};
 
@@ -31,33 +20,69 @@ Intern& Intern::operator=(const Intern &rhs)
 	return *this;
 }
 
-AForm* Intern::makeForm(std::string formName, std::string target)
+AForm* Intern::makeForm(const std::string formName, const std::string target)
 {
-	(void) formName;
-	(void) target;
+	const std::string tab[3] = {"ShrubberyCreationForm", "RobotomyRequestForm", "PresidentialPardonForm"};
+	int id_form;
+	int i;
+	AForm *tabForms[3];
+	
+	id_form = -1;
+	i = 0;
+	tabForms[0] = new ShrubberyCreationForm(formName, target); 
+	tabForms[1] = new RobotomyRequestForm(formName, target); 
+	tabForms[2] = new PresidentialPardonForm(formName, target); 
 
-	AForm *form00 = NULL;
-
-	std::cout << strcmp("bk", "bk") << std::endl;
-
-
-	if (strcmp("bk", "bk") == 0)
+	while (i < 3)
 	{
-		std::cout << "OUI" << std::endl;
+		if (tab[i].compare(formName) != 0)
+			delete tabForms[i];
+		else
+			id_form = i;
+		i++;
 	}
 
-	return form00;
-
+	if (id_form != -1)
+	{
+		std::cout << "Intern creates " << formName << std::endl;
+		return tabForms[id_form];
+	}
+	std::cout << "Intern can not creates " << formName << std::endl;
+	return nullptr;
 }
 
 int main()
 {
+	
+	Intern		james;
+	Bureaucrat	parmelin("Guy Parmelin", 1);
 
-	Intern intern0;
+	std::cout << "------------------ Test Robotomy ------------------" << std::endl;
+	AForm *RobotomyRequestForm0 = james.makeForm("RobotomyRequestForm","42 Lausanne");
+	parmelin.signForm(*RobotomyRequestForm0);
+	parmelin.executeForm(*RobotomyRequestForm0);
+	delete RobotomyRequestForm0;
+	std::cout << std::endl;
 
-	intern0.makeForm("","");
+	std::cout << "------------------ Test Shrubbery ------------------" << std::endl;
+	AForm *ShrubberyCreationForm0 = james.makeForm("ShrubberyCreationForm","Saxon");
+	parmelin.signForm(*ShrubberyCreationForm0);
+	parmelin.executeForm(*ShrubberyCreationForm0);
+	delete ShrubberyCreationForm0;
+	std::cout << std::endl;
 
 
+	std::cout << "------------------ Test Presidential Pardon ------------------" << std::endl;
+	AForm *PresidentialPardonForm0 = james.makeForm("PresidentialPardonForm","Ford Perfect");
+	parmelin.signForm(*PresidentialPardonForm0);
+	parmelin.executeForm(*PresidentialPardonForm0);
+	delete PresidentialPardonForm0;
+	std::cout << std::endl;	
+
+	std::cout << "------------------ Test False Form ------------------" << std::endl;
+	AForm *FalseForm0 = james.makeForm("FalseForm","Bale");
+	(void) FalseForm0;
+	std::cout << std::endl;	
 
 	return 0;
 }
